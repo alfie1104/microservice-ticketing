f3ad6b4608c87a48aa0551bc9e1f9bf01c851f5c
alfie1104 <alfie1104@gmail.com>
2023-04-14 10:21:39 +0900

    feat(package.json): modify setting for ts-node-dev

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -6,7 +6,7 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong", err);
+  //console.log("Something went wrong", err);
 
   res.status(400).send({
     message: err.message,
