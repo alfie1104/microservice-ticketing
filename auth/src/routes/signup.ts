6a8fc6bfc0cde625742a5f375ce32b8faa054b60
alfie1104 <alfie1104@gmail.com>
2023-04-14 09:16:04 +0900

    feat(error-handler): building an error handling middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -16,12 +16,17 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      const error = new Error("Invalid email or password");
+      //error.reasons = errors.array();
+
+      throw error;
     }
 
     const { email, password } = req.body;
 
     console.log("Creating a user...");
+
+    throw new Error("Error connecting to database");
 
     res.send({});
 
