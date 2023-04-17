447448279fde5a3e94333d058d91ec3da958d0fb
alfie1104 <alfie1104@gmail.com>
2023-04-18 08:55:37 +0900

    feat(express-async-erros): async error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -15,9 +16,10 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all("*", () => {
+app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
+
 app.use(errorHandler);
 
 app.listen(3000, () => {
