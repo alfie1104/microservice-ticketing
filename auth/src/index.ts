225f950fc259cb7c9f07192a78ca57826a7931e7
alfie1104 <alfie1104@gmail.com>
2023-04-25 08:53:44 +0900

    feat(signup, index): adding session support using cookie-session and apply JWT signing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -11,7 +12,20 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+/* 
+  traffic is being proxied to our application through Ingress EngineX
+  So we are just adding 'app.set("trust proxy", true) to make sure that Express is aware
+  that it's behind a proxy of Ingree Engine X, and to make sure that it should still trust traffic as being secure,
+  event though it's comming from that proxy
+*/
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
