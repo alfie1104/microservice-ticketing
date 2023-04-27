80f27e15baf9b739c3b71f4c27bbbf2ad7db7066
alfie1104 <alfie1104@gmail.com>
2023-04-28 08:59:48 +0900

    feat(app, index, dockerfile): add few dependencies for testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,42 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-/* 
-  traffic is being proxied to our application through Ingress EngineX
-  So we are just adding 'app.set("trust proxy", true) to make sure that Express is aware
-  that it's behind a proxy of Ingree Engine X, and to make sure that it should still trust traffic as being secure,
-  event though it's comming from that proxy
-*/
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
