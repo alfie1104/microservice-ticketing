616176ebb9d85aaf35ec128ffb6da67ec8656ba8
alfie1104 <alfie1104@gmail.com>
2023-05-12 08:56:40 +0900

    feat(client/pages/index): fetching current user data during SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,16 @@
-const LandingPage = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get("/api/users/currentuser").catch((err) => {
+    console.log(err.message);
+  });
+
+  return response ? response.data : {};
 };
 
 export default LandingPage;
