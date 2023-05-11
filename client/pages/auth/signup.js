e9d6f9b9b2a4afea5d182b6372b9bd7153abbbcd
alfie1104 <alfie1104@gmail.com>
2023-05-12 08:45:46 +0900

    feat(client/userequest, signup): add onSuccess callback on useRequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
 import useRequest from "../../hooks/useRequest";
 
 const SignUp = () => {
@@ -12,9 +12,10 @@ const SignUp = () => {
       email,
       password,
     },
+    onSuccess: () => Router.push("/"),
   });
 
-  const onSubmit = async (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     doRequest();
