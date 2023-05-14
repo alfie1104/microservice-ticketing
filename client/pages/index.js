74b7d5a76e7d67d29a7ac5d4be5b84f556f4d569
alfie1104 <alfie1104@gmail.com>
2023-05-15 08:54:17 +0900

    feat(client/pages/index): passing through the cookies in getInitialProps to get currentUser

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,12 +5,31 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios.get("/api/users/currentuser").catch((err) => {
-    console.log(err.message);
-  });
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    //we are on the server!
+    //requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser
+    const response = await axios
+      .get(
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+        {
+          headers: req.headers,
+        }
+      )
+      .catch((err) => {
+        console.log(err.message);
+      });
 
-  return response ? response.data : {};
+    return response ? response.data : {};
+  } else {
+    //we are on the browser!
+    //requests can be made with a base url of ''
+    const response = await axios.get("/api/users/currentuser").catch((err) => {
+      console.log(err.message);
+    });
+
+    return response ? response.data : {};
+  }
 };
 
 export default LandingPage;
