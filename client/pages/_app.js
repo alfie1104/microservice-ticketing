bc13d9439f0ae9089bc0ae765ff6f3eec96dca83
alfie1104 <alfie1104@gmail.com>
2023-05-16 08:48:07 +0900

    feat(pages/_app): handling multiple getInitialProps and passing props through to component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,30 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header! {currentUser?.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const response = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    //개별 Page에 getInitialProps을 설정해놨을 경우 호출
+    //이렇게 하지 않으면, _app.js에 있는 getInitialProps만 호출됨 (_app.js에 getInitialProps을 설정했으므로 우선 호출됨)
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...response.data,
+  };
+};
+
+export default AppComponent;
