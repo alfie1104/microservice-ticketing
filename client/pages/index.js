fb00b91063448d9a3c13df34c0b171aca631ffe1
alfie1104 <alfie1104@gmail.com>
2023-05-16 07:57:26 +0900

    feat(api/build-client, pages/index): create a reusable API client and modify landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,35 +1,17 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    //we are on the server!
-    //requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser
-    const response = await axios
-      .get(
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-        {
-          headers: req.headers,
-        }
-      )
-      .catch((err) => {
-        console.log(err.message);
-      });
-
-    return response ? response.data : {};
-  } else {
-    //we are on the browser!
-    //requests can be made with a base url of ''
-    const response = await axios.get("/api/users/currentuser").catch((err) => {
-      console.log(err.message);
-    });
-
-    return response ? response.data : {};
-  }
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const response = await client.get("/api/users/currentuser");
+  return response ? response.data : {};
 };
 
 export default LandingPage;
